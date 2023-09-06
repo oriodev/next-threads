1fd87bd5e0a9a40fa7ae46d9c8027d2f9464ca62
ori <93719767+oriodev@users.noreply.github.com>
2023-09-06 12:28:40 +0100

    finished onboarding & uploadthing

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -17,8 +17,10 @@ import * as z from 'zod'
 import { Input } from '../ui/input'
 import { Button } from '../ui/button'
 import Image from 'next/image'
-import { ChangeEvent } from 'react'
+import { ChangeEvent, useState } from 'react'
 import { Textarea } from '../ui/textarea'
+import { isBase64Image } from '@/lib/utils'
+import { useUploadThing } from '@/lib/uploadthing'
 
 interface Props {
   user: {
@@ -34,6 +36,10 @@ interface Props {
 
 const AccountProfile = ({ user, btnTitle }: Props) => {
 
+  const [files, setFiles] = useState<File[]>([])
+
+  const { startUpload } = useUploadThing('media')
+
   const form = useForm({
     resolver: zodResolver(userValidation),
     defaultValues: {
@@ -44,14 +50,45 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
     }
   })
 
-  const handleImage = (e: ChangeEvent, fieldChange: (value: string) => void) => {
+  const handleImage = (e: ChangeEvent<HTMLInputElement>, fieldChange: (value: string) => void) => {
     e.preventDefault()
+
+    const fileReader = new FileReader()
+
+    if ( e.target.files && e.target.files.length > 0 ) {
+      const file = e.target.files[0]
+      setFiles(Array.from(e.target.files))
+
+      if (!file.type.includes('image') ) return 
+
+      fileReader.onload = async (event) => {
+        const imageDataUrl = event.target?.result?.toString() || ''
+
+        fieldChange(imageDataUrl)
+      }
+
+      fileReader.readAsDataURL(file)
+
+    }
+
   }
 
-  function onSubmit(values: z.infer<typeof userValidation>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
+  const onSubmit = async function (values: z.infer<typeof userValidation>) {
+    const blob = values.profile_photo
+
+    const hasImageChanged = isBase64Image(blob)
+
+    if (hasImageChanged) {
+      const imgRes = await startUpload(files)
+
+      if (imgRes && imgRes[0].fileUrl) {
+        values.profile_photo = imgRes[0].fileUrl
+      }
+
+    }
+
+    // TODO: UPDATE USER PROFILE
+
   }
 
   return (

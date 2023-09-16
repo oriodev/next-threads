22efc0f61e24ac227e073a232342a53675eee6d3
ori <93719767+oriodev@users.noreply.github.com>
2023-09-16 20:53:07 +0100

    fixed an error

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -21,6 +21,9 @@ import { ChangeEvent, useState } from 'react'
 import { Textarea } from '../ui/textarea'
 import { isBase64Image } from '@/lib/utils'
 import { useUploadThing } from '@/lib/uploadthing'
+import { updateUser } from '@/lib/actions/user.actions'
+
+import { usePathname, useRouter } from 'next/navigation'
 
 interface Props {
   user: {
@@ -37,8 +40,9 @@ interface Props {
 const AccountProfile = ({ user, btnTitle }: Props) => {
 
   const [files, setFiles] = useState<File[]>([])
-
   const { startUpload } = useUploadThing('media')
+  const router = useRouter()
+  const pathname = usePathname()
 
   const form = useForm({
     resolver: zodResolver(userValidation),
@@ -87,7 +91,14 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
 
     }
 
-    // TODO: UPDATE USER PROFILE
+    await updateUser({
+      username: values.username,
+      name: values.name,
+      bio: values.bio,
+      image: values.profile_photo,
+      userId: user.id,
+      path: pathname
+    })
 
   }
 
